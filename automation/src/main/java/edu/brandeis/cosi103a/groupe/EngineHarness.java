35221ff76735af77d72d0aeb0c96cee2268fd5b2
aarong723 <aargold@icloud.com>
2025-02-25 12:16:00 -0500

    used for code review

--- automation/src/main/java/edu/brandeis/cosi103a/groupe/EngineHarness.java
+++ automation/src/main/java/edu/brandeis/cosi103a/groupe/EngineHarness.java
@@ -2,6 +2,7 @@ package edu.brandeis.cosi103a.groupe;
 
 import edu.brandeis.cosi.atg.api.Engine;
 
+//Used for code review
 public class EngineHarness {
     public static void main(String[] args) {
     Engine newEngine = GameEngine.makeEngine(null, null, null);
