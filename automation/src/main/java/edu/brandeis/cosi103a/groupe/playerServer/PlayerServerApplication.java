a49361bd6ade8ea458efd283f1b5fb8e3df72f70
EmSzabo <emilyszabo@brandeis.edu>
2025-04-28 08:43:22 -0400

    Update PlayerServerApplication.java

--- automation/src/main/java/edu/brandeis/cosi103a/groupe/playerServer/PlayerServerApplication.java
+++ automation/src/main/java/edu/brandeis/cosi103a/groupe/playerServer/PlayerServerApplication.java
@@ -12,7 +12,6 @@ public class PlayerServerApplication {
     /** 
      * @param args
      */   
-     main
     public static void main(String[] args) {
         SpringApplication.run(PlayerServerApplication.class, args);
     }
